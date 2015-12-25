7640c142408542d06783d9406d18e32d71238d62
Daniel Kullmann <daniel.kullmann@sap.com>
2015-12-25 11:36:54 +0100

    Failed attempt (takes too tmuch time) to solve second problem of day 19.

--- 19a.py
+++ 19a.py
@@ -1,5 +1,5 @@
 rules = []
-input = None
+target = None
 fh = open('19.txt', 'r')
 for line in fh.readlines():
     line = line.strip()
@@ -7,23 +7,35 @@ for line in fh.readlines():
     if len(parts) == 2:
       rules.append(parts)
     else:
-      input = line
+      target = line
 
-def process(input, rules):
+def process(target, rules):
     results = set()
     for (a,b) in rules:
-      strIndex = input.find(a)
+      strIndex = target.find(a)
       while strIndex >= 0:
-          newStr = input[0:strIndex] + b + input[strIndex+len(a):]
+          newStr = target[0:strIndex] + b + target[strIndex+len(a):]
           results.add(newStr)
-          strIndex = input.find(a, strIndex+1)
+          strIndex = target.find(a, strIndex+1)
     return list(results)
 
+def longMatch(string):
+  result = 0
+  while string[result] == target[result] and result < len(string) and result < len(target):
+    result += 1
+  return result
+
+print len(target)
 molecules = [(0,'e')]
-while molecules[0][1] != input:
+count = 0
+while molecules[0][1] != target:
     molecule = molecules[0]
     molecules = molecules[1:]
     for newMolecule in process(molecule[1], rules):
         molecules.append((molecule[0]+1,newMolecule))
-    #print len(molecules), min(map(len,map(lambda x: x[1], molecules)))
+    molecules.sort(key=lambda x: x[0] - longMatch(x[1]))
+    if len(molecules) > 5000: molecules = molecules[0:2500]
+    count += 1
+    if count %100 == 0:
+      print len(molecules), longMatch(molecules[0][1]), molecules[0]
 print molecules[0]
